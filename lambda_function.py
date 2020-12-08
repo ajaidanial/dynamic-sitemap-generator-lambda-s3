447ca3b75db77ad5915318da4f92d14dc747ffb6
ajaidanial <ajai@geopits.com>
2020-12-08 18:44:39 +0530

    finished the conding

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,17 @@
 import json
 import os
-from .libraries.upload_file import upload_to_aws
+
+import boto3
+
+
+def upload_to_aws(local_file, bucket, s3_file, aws_access_key, aws_secret_key):
+    """Uploads the given file to AWS S3 using the given credentials and config."""
+
+    s3 = boto3.client(
+        "s3", aws_access_key_id=aws_access_key, aws_secret_access_key=aws_secret_key
+    )
+    s3.upload_file(local_file, bucket, s3_file, ExtraArgs={"ACL": "public-read"})
+    print("File Upload Successful")
 
 
 def generate_sitemap():
